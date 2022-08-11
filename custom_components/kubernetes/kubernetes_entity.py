197fd4c9e96c9eb2754418db757548dbad1e7df5
Alistair Young <avatar@arkane-systems.net>
2022-08-11 00:46:01 -0500

    Fixes.

--- custom_components/kubernetes/kubernetes_entity.py
+++ custom_components/kubernetes/kubernetes_entity.py
@@ -64,5 +64,4 @@ class KubernetesEntity(Entity):
 
     @property
     def extra_state_attributes(self) -> dict:
-        dict = { "raw" : obj_to_dict(self.getData()) }
-        return 
+        return { "raw" : obj_to_dict(self.getData()) }
